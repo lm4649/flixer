d0369c26a26fb10c8b687ed6f8f18b88a4155daf
marc lopes <marc_lopes2004@yahoo.fr>
2020-09-07 23:25:42 +0900

    fecth tmdb first step

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Header } from './components';
 import { Home } from './routes';
+import { API_URL, API_KEY } from './config';
 import './App.css';
 
 class App extends Component {
@@ -55,6 +57,20 @@ class App extends Component {
         title: "Joker"
       }
     ]
+  }
+
+  async componentDidMount() {
+    try{
+      this.loadMovies();
+    } catch(e) {
+      console.log('load Movies failed', e);
+    }
+  }
+
+  loadMovies = () => {
+    const page = this.state.activePage + 1;
+    const url = `${API_URL}/movie/popular?api_key${API_KEY}&page=${page}&language=en-US`;
+    return axios.get(url);
   }
 
   handleSearch = value => console.log('handleSearch', value);
