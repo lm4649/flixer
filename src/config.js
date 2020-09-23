865e1a1c54967bf98d8957055b35130220e885da
marc lopes <marc_lopes2004@yahoo.fr>
2020-09-23 11:30:53 +0900

    set env var

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const API_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
+const BACKDROP_SIZE = 'w1280';
+const POSTER_SIZE='w500';
+
+export {
+        API_URL,
+        IMAGE_BASE_URL,
+        BACKDROP_SIZE,
+        POSTER_SIZE
+       };
